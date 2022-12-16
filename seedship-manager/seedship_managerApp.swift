dc254148ccc5ea8f048e7966e09aba4b16bc5c6e
Alex <alex.viney@biteable.com>
2022-12-16 20:12:35 +1100

    Initial Commit

---
+++ seedship-manager/seedship_managerApp.swift
@@ -0,0 +1,17 @@
+//
+//  seedship_managerApp.swift
+//  seedship-manager
+//
+//  Created by Alex Viney on 16/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct seedship_managerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
